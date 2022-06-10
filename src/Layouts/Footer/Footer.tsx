b89cfc46b5bc84fbd44410ed3f1ab9f9e8b706a3
nPhong <phongn@itsj-group.com>
2022-06-10 17:08:28 +0700

    side bar

--- src/Layouts/Footer/Footer.tsx
+++ src/Layouts/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import { Col, Container, Row } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+  const { t } = useTranslation('web_app');
   return (
     <Container>
       <Row>
-        <Col>This is footer.</Col>
+        <Col>{t('web-app.footer')}</Col>
       </Row>
     </Container>
    );
--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -1,11 +1,36 @@
-import Menu from '../Menu/Menu';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import logo from '../../logo.svg';
+import './Header.scss';
+import { useTranslation } from 'react-i18next';
+import i18n from '../../Config/i18n/i18n';
+import { useState } from 'react';
 
 function Header() {
+  const { t } = useTranslation('web_app');
+  const [lang, setLang] = useState('jp');
+
+  const handleSelect = (eventKey: any) => {
+    i18n.changeLanguage(eventKey);
+    setLang(eventKey);
+  };
+
   return (
     <>
-      <Menu></Menu>
+      <Navbar bg="primary" variant="dark" onSelect={handleSelect}>
+        <Container>
+          <Navbar.Brand href="/"><img src={logo} className="App-logo mlogo" alt="logo" /></Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link href="/signIn">{t('web-app.signin')}</Nav.Link>
+            <Nav.Link href="/signUp">{t('web-app.signup')}</Nav.Link>
+            <NavDropdown title={lang} id="nav-dropdown">
+              <NavDropdown.Item eventKey="en">English</NavDropdown.Item>
+              <NavDropdown.Item eventKey="jp">Japan</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
-   );
+  );
 }
 
 export default Header;
