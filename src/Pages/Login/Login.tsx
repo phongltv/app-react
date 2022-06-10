65da6a2118f3173aada08a7b9b74cf4f96867bb2
nPhong <phongn@itsj-group.com>
2022-06-10 10:46:41 +0700

    login change

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -4,7 +4,7 @@ function Login() {
   return (
     <Container>
       <Row>
-        <Col className='text-center'>Login page</Col>
+        <Col className='text-center'>Login page 1</Col>
       </Row>
     </Container>
    );
